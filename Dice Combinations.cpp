b012e4553e7476298827fe9e6988abfe72d0d423
Souradip Mandal <67626543+deep2609@users.noreply.github.com>
2020-10-24 11:18:09 +0530

    Update Dice Combinations.cpp

--- Dice Combinations.cpp
+++ Dice Combinations.cpp
@@ -6,12 +6,12 @@ int main(){
       boost;
       int n;
       cin>>n;
-      int dp[n+1];
-      dp[0]=1;
+      int dp[n+1];                             //dp[i] stores the number of ways to construct sum 'i'
+      dp[0]=1;                                 //initialise dp[0]=1
       for(int i=1;i<=n;i++){
          dp[i]=0;
-         for(int j=1;j<=min(i,6);j++){
-           dp[i]+=dp[i-j];
+         for(int j=1;j<=min(i,6);j++){         // iterating over the last number that was added to construct sum "i"
+           dp[i]+=dp[i-j];                     
            dp[i]%=mod;
          }
        }
